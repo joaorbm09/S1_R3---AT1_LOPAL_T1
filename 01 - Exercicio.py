d1dadf75de82f57fd034dc300017efc5cd065726
joaorbm09 <joao.rbm09@gmail.com>
2026-05-20 21:06:28 -0300

    S1_R3---AT1_LOPAL_T1

--- 01 - Exercicio.py
+++ 01 - Exercicio.py
@@ -1,10 +1,6 @@
-# Loop de 0 a 100, incluindo o número 100
 for i in range(101):
-    # Verifica se o número atual é divisível por 2
     if i % 2 == 0:
-        # Se for par, imprime que o número é par
         print(f"O número ({i}) é Par")
     else:
-        # Se não for par, imprime que o número é ímpar
         print(f"O número ({i}) é impar")
 
